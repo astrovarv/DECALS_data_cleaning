c4b0011f01372fef8d4ba1637eebf0a3d5cbeeb0
Varvara <74026346+astrovarv@users.noreply.github.com>
2021-04-25 11:57:58 +0100

    Add files via upload

---
+++ scripts/north_shift.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  5 15:12:19 2021
+
+@author: apple
+"""
+
+import astropy.io.fits as fits
+from astropy.table import Table, hstack, vstack
+import pandas as pd
+
+df = pd.read_csv('all_objects.csv')
+
+df = df[(df['FLUX_G'] > 0) & (df['FLUX_R'] > 0) & (df['FLUX_Z'] > 0) & (df['FLUX_W1'] > 0) & (df['FLUX_W2'] > 0)] 
+
+def shift(gflux, rflux, zflux):
+    gshift = gflux * 10**(-0.4*0.004) * (gflux/rflux)**(-0.059)
+    rshift = rflux * 10**(0.4*0.003) * (rflux/zflux)**(-0.024)
+    zshift = zflux * 10**(0.4*0.013) * (rflux/zflux)**(+0.015)
+    return gshift, rshift, zshift
+
+north = df[df['HEMISPHERE'] == 'N']
+g, r, z = shift(north['FLUX_G'], north['FLUX_R'], north['FLUX_Z'])
+
+
+north['FLUX_G'] = g
+north['FLUX_R'] = r
+north['FLUX_Z'] = z
+south = df[df['HEMISPHERE'] == 'S']
+df = pd.concat([south, north], ignore_index=True)
+
+df.to_csv('all_objects.csv', index = False)
